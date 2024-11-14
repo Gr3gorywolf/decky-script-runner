07f1fb46c80df075d4a2a16c623d08eb4e682f7d
Gregory Cabral De Los Santos <gregory@devour.io>
2024-11-14 00:46:20 -0400

    Renamed script viewer

--- src/components/ScriptConsoleModal.tsx
+++ src/components/ScriptConsoleModal.tsx
@@ -6,78 +6,86 @@ import { call } from "@decky/api";
 import React from "react";
 
 interface props {
-  script: ScriptData;
-  closeModal?: () => void;
+    script: ScriptData;
+    isRunning: boolean;
+    closeModal?: () => void;
 }
 
-export const ScriptConsoleModal: FC<props> = ({ closeModal, script }) => {
-  const [consoleOutput, setConsoleOutput] = useState<string[]>([]);
-  const containerRef = React.useRef<HTMLDivElement>(null);
-  const fetchConsoleOutput = async () => {
-    let scriptLogs = await call<[string], string>("get_script_logs", script.name);
-    setConsoleOutput(scriptLogs.split("\n"));
-   
-  }
-
-  const autoScroll = () =>{
-    const container = containerRef.current;
-    if(!container) return;
-    const offset = 2.0;
-    const reverseScrollHeight = Math.abs(container.scrollHeight - container.scrollTop - container.clientHeight);
-    const isOnBottom = reverseScrollHeight <= offset;
-    if(isOnBottom ) {
-      setTimeout(() => {
-        container.scrollTop = 10000000;
-      }, 15);
-    }
-  }
-
-  useEffect(() => {
-    const interval = setInterval(fetchConsoleOutput, 1000);
-    const autoScrollInterval = setInterval(autoScroll, 200);
-    return () => {
-      clearInterval(interval);
-      clearInterval(autoScrollInterval);
+export const ScriptConsoleModal: FC<props> = ({ closeModal, script, isRunning }) => {
+    const [consoleOutput, setConsoleOutput] = useState<string[]>([]);
+    const containerRef = React.useRef<HTMLDivElement>(null);
+    const fetchConsoleOutput = async () => {
+        let scriptLogs = await call<[string], string>("get_script_logs", script.name);
+        setConsoleOutput(scriptLogs.split("\n"));
     };
-  }, []);
 
-  const consoleStyle = {
-    color: "#ffffff",
-    fontFamily: "monospace",
-    padding: "5px",
-    borderRadius: "5px",
-    maxWidth: "600px",
-    width: "100%",
-    overflow: "auto",
-    minHeight: "240px",
-    maxHeight: "240px",
-  };
+    const autoScroll = () => {
+        const container = containerRef.current;
+        if (!container) return;
+        const offset = 2.0;
+        const reverseScrollHeight = Math.abs(container.scrollHeight - container.scrollTop - container.clientHeight);
+        const isOnBottom = reverseScrollHeight <= offset;
+        if (isOnBottom) {
+            setTimeout(() => {
+                container.scrollTop = 10000000;
+            }, 15);
+        }
+    };
 
-  const lineStyle = {
-    margin: "0",
-    padding: "2px 0",
-    display: "flex",
-  };
+    useEffect(() => {
+        const interval = setInterval(fetchConsoleOutput, 1000);
+        const autoScrollInterval = setInterval(autoScroll, 200);
+        return () => {
+            clearInterval(interval);
+            clearInterval(autoScrollInterval);
+        };
+    }, []);
 
-  const promptStyle = {
-    color: "#00ff00",
-    marginRight: "8px",
-  };
+    const consoleStyle = {
+        color: "#ffffff",
+        fontFamily: "monospace",
+        padding: "5px",
+        borderRadius: "5px",
+        maxWidth: "600px",
+        width: "100%",
+        overflow: "auto",
+        minHeight: "240px",
+        maxHeight: "240px",
+    };
 
-  const infoStyle = {
-    color: 'white',
-  };
+    const lineStyle = {
+        margin: "0",
+        padding: "2px 0",
+        display: "flex",
+    };
 
-  return (
-    <ConfirmModal closeModal={closeModal} bAlertDialog={true}  strTitle={<>Running: {humanizeFileName(script.name)}</>}>
-      <div style={consoleStyle} ref={containerRef}>
-        {consoleOutput.map((line, index) => (
-          <div key={index} style={lineStyle}>
-            <span style={promptStyle}>{`/>`}</span>
-            <span style={infoStyle}>{line}</span>
-          </div>
-        ))}
-      </div>
-    </ConfirmModal>
-  );
+    const promptStyle = {
+        color: "#00ff00",
+        marginRight: "8px",
+    };
+
+    const infoStyle = {
+        color: "white",
+    };
+
+    return (
+        <ConfirmModal
+            closeModal={closeModal}
+            bAlertDialog={true}
+            strTitle={
+                <>
+                    {isRunning ? "Output of" : "Logs of"}: {humanizeFileName(script.name)}
+                </>
+            }
+        >
+            <div style={consoleStyle} ref={containerRef}>
+                {consoleOutput.map((line, index) => (
+                    <div key={index} style={lineStyle}>
+                        <span style={promptStyle}>{`/>`}</span>
+                        <span style={infoStyle}>{line}</span>
+                    </div>
+                ))}
+            </div>
+        </ConfirmModal>
+    );
 };
---
+++ src/components/ScriptViewerModal.tsx
@@ -0,0 +1,66 @@
+import { FC, useEffect, useState } from "react";
+import { ScriptData } from "../types/script-data";
+import { ConfirmModal } from "@decky/ui";
+import { call} from "@decky/api";
+import React from "react";
+import { CodeBlock, vs2015 } from "react-code-blocks";
+import { getFileExtension } from "../utils/helpers";
+
+interface props {
+    script: ScriptData;
+    closeModal?: () => void;
+}
+
+export const ScriptViewerModal: FC<props> = ({ closeModal, script }) => {
+    const [content, setContent] = useState("");
+    const containerRef = React.useRef<HTMLDivElement>(null);
+    const fetchScriptContent = async () => {
+        const content = await call<[string], string>("get_script_content", script.name);
+        setContent(content);
+    };
+
+    useEffect(() => {
+        fetchScriptContent();
+    }, []);
+
+    const editorStyle = {
+        padding: "5px",
+        borderRadius: "5px",
+        maxWidth: "600px",
+        width: "100%",
+        overflow: "auto",
+        minHeight: "240px",
+        maxHeight: "240px",
+    };
+
+    const languagesByExtension = {
+        ".sh": "bash",
+        ".lua": "lua",
+        ".js": "javascript",
+        ".pl": "perl",
+        ".php": "php",
+        ".py": "python",
+        ".rb": "ruby",
+    };
+
+    const extension = getFileExtension(script.name);
+    let language = undefined;
+    if (extension) {
+        // @ts-ignore
+        language = languagesByExtension[extension] ?? undefined;
+    }
+
+    return (
+        <ConfirmModal
+            closeModal={closeModal}
+            onCancel={closeModal}
+            strCancelButtonText="Close"
+            bAlertDialog={true}
+            strTitle={<>Viewing: {script.name}</>}
+        >
+            <div style={editorStyle} ref={containerRef}>
+                <CodeBlock text={content} language={language}  showLineNumbers={true} theme={vs2015} />
+            </div>
+        </ConfirmModal>
+    );
+};
